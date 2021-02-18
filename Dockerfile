7adbfdf569f15cac3d2c283afe453eaca51e6940
bsegault <8903454+bsegault@users.noreply.github.com>
2021-02-18 17:21:00 +0100

    Use log file and volumes for persistence

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM node:15-alpine
 
 # Leave some public information for potential users
 LABEL maintainer="Benjamin Segault <benjamin.segault@gmail.com>"
-LABEL version="v0.0.4"
+LABEL version="v0.0.5"
 LABEL description="Simple HTTP server based on Node.js to illustrate Docker principles"
 
 # Define current directory (created automatically)
@@ -11,11 +11,15 @@ WORKDIR /app
 # Expose default port used by the container
 EXPOSE 80
 
-# Better: environment is used for configuring the app, with a default
-# Users will now that they can configure this if they want
+# Environment is used for configuring the app, with a default
+# Users will know that they can configure this if they want
 ENV MAX_HEADER_SIZE=4096
+
+# Better: default volume for log files: persitence even if user did not configured it
+VOLUME ["/var/logs"]
 
 COPY index.js ./
 
-# Better: add default arguments to CMD
-CMD ["node", "index.js", "4096"]
+# CMD to give default arguments
+#   Here we could argue that this should be part of the environment.
+CMD ["node", "index.js", "--log-file", "/var/logs/docker_http.log"]
--- run_server_container.sh
+++ run_server_container.sh
@@ -6,18 +6,23 @@ set -x
 
 # Build image and name it bsegault/docker-http
 # We tag it so we now what's inside without inspecting
-docker build -t bsegault/docker-http:04_smart_env .
+docker build -t bsegault/docker-http:05_smarter_volumes .
 
 # -d: Run the container in detached mode
 # -p: Map host port 8080 to container port 80
 # --name: give a name to the container to manage it
-# Better: --env: give options by env: no need to know the arguments taken by the app; we have the meaning by variable name
+# --env: give options by env: no need to know the arguments taken by the app; we have the meaning by variable name
 #   No need to even document the program, just detail env vars.
+# Better: --mount: We can inspect logs by reading bind mount. Take care to re-use pre-defined bind mounts.
+#   Here we mount it to a folder in source tree: take care to add this folder to .dockerignore
+# Give log file location in bind mount as program argument:
+#   Here we could argue that this should be part of the environment. This will be kept to illustrate various use-cases later on.
 docker run -d \
     -p 8080:80 \
     --name docker_http \
     --env MAX_HEADER_SIZE=4096 \
-    bsegault/docker-http:04_smart_env
+    --mount "type=bind,src=${PWD}/logs,dst=/var/logs" \
+    bsegault/docker-http:05_smarter_volumes --log-file /var/logs/docker_http.log
 
 # Wait a bit
 sleep 1
@@ -27,3 +32,6 @@ curl http://localhost:8080
 
 # Cleanup!
 docker rm -f docker_http
+
+# Better: watch logs even after container deletion
+cat "${PWD}/logs/docker_http.log"
