fdc07b4e21d806ad5f2f9cf1b7ab97454d598870
bsegault <8903454+bsegault@users.noreply.github.com>
2021-02-18 17:21:01 +0100

    More complex npm app using express

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM node:alpine
 
 # Leave some public information for potential users
 LABEL maintainer="Benjamin Segault <benjamin.segault@gmail.com>"
-LABEL version="v0.0.5"
+LABEL version="v0.0.6"
 LABEL description="Simple HTTP server based on Node.js to illustrate Docker principles"
 
 # Define current directory (created automatically)
@@ -15,13 +15,19 @@ EXPOSE 80
 # Users will know that they can configure this if they want
 ENV MAX_HEADER_SIZE=4096
 
+ENV NODE_ENV=production
+
 # Better: default volume for log files: persitence even if user did not configured it
 VOLUME ["/var/logs"]
 
-COPY index.js ./
+COPY index.js package.json package-lock.json ./
+
+# Install dependencies
+RUN npm ci
 
 # Use of ENTRYPOINT to define default command that is not easily overriden
-ENTRYPOINT ["node", "index.js"]
+# Better: single definition of start command in package.json
+ENTRYPOINT ["npm", "start"]
 
 # CMD to give default arguments
 #   Here we could argue that this should be part of the environment.
--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 const fs = require('fs')
 const http = require('http')
 
+// Rely on third-party module express rather than built-in Node.js "http" module.
+const express = require('express')
+
+const app = express()
+
 // Static config as a constant on top of app
 // This should always be constant, so not really an issue to write here and in Dockerfile.
 const port = 80
 
 // Server config can be retrieved from environment. Use a default to avoid early failure if value is not set
 const maxHeaderSize = Number.parseInt(process.env.MAX_HEADER_SIZE || 4096)
+const htmlFolder = process.env.HTML_FOLDER || "./assets"
 
-// Better: parameter to define optional log file
+const helloWorldMessage = 'Hello World!\n'
+
+// Parameter to define optional log file
 // Here we could argue that this should be part of the environment.
 let logFile = 2 // 2 is stdout
 let logFileArgIndex = process.argv.indexOf('--log-file')
@@ -16,23 +24,31 @@ if (logFileArgIndex !== -1) {
   logFile = fs.openSync(process.argv[logFileArgIndex + 1], 'w')
 }
 
-// Start log message
-fs.writeSync(
-  logFile,
-  `Starting server on port ${port}; maxHeaderSize is ${maxHeaderSize}\n`
-)
+// Better: Log function
+const logRequest = (req, res, next) => {
+  // Some log messages
+  // Log to a file meant to be readable outside of Docker
+  fs.writeSync(logFile, `[${new Date().toDateString()}] Request from ${req.socket.localAddress}: ${req.url}\n`)
+  next()
+}
 
-http
-  .createServer({ maxHeaderSize: maxHeaderSize }, (req, res) => {
-    const message = 'Hello World!\n'
-    res.write(message)
-    res.end()
+// Go through log function before anything
+app.use(logRequest)
 
-    // Some log messages
-    // Better: log to a file meant to be readable outside of Docker
-    fs.writeSync(
-      logFile,
-      `Request from ${req.socket.localAddress}; Sent ${message}\n`
-    )
-  })
-  .listen(port)
+// Sample custom route: /hello
+app.get('/hello', (req, res) => {
+  fs.writeFileSync(logFile, `Sending ${helloWorldMessage}`)
+  res.send(helloWorldMessage)
+  res.end()
+})
+
+// Allow to serve static html content: index.html is served by default when requesting /
+app.use(express.static(htmlFolder))
+
+http.createServer({ maxHeaderSize: maxHeaderSize }, app).listen(port, () => {
+  // Start log message
+  fs.writeSync(
+    logFile,
+    `[${new Date().toDateString()}] Starting server on port ${port}; maxHeaderSize is ${maxHeaderSize}\n`
+  )
+})
