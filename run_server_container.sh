e8cee03e9ea4280452847d40da552ad9c73072bf
bsegault <8903454+bsegault@users.noreply.github.com>
2021-02-18 17:20:57 +0100

    Docker structure improvements

--- run_server_container.sh
+++ run_server_container.sh
@@ -5,14 +5,15 @@ set -x
 
 
 # Build image and name it bsegault/docker-http
-docker build -t bsegault/docker-http .
+# Better: we tag it so we now what's inside without inspecting
+docker build -t bsegault/docker-http:02_dummy .
 
 # -d: Run the container in detached mode
 # -p: Map host port 8080 to container por 80
-# Run "node /index.js" from our bsegault/docker-http Docker image
+# Better: Start the image with its default command. No need to know what it is using and how it is built
 docker run -d \
     -p 8080:80 \
-    bsegault/docker-http node /index.js
+    bsegault/docker-http:02_dummy
 
 # Wait a bit
 sleep 1
