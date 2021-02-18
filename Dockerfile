1e5aa7e79601cc68cad272d5f4c4bd6a564b24e5
bsegault <8903454+bsegault@users.noreply.github.com>
2021-02-18 17:21:00 +0100

    Use log file and volumes for persistence

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM node:alpine
 
 # Leave some public information for potential users
 LABEL maintainer="Benjamin Segault <benjamin.segault@gmail.com>"
-LABEL version="v0.0.4"
+LABEL version="v0.0.5"
 LABEL description="Simple HTTP server based on Node.js to illustrate Docker principles"
 
 # Define current directory (created automatically)
@@ -11,11 +11,18 @@ WORKDIR /app
 # Expose default port used by the container
 EXPOSE 80
 
-# Better: environment is used for configuring the app, with a default
-# Users will now that they can configure this if they want
+# Environment is used for configuring the app, with a default
+# Users will know that they can configure this if they want
 ENV MAX_HEADER_SIZE=4096
+
+# Better: default volume for log files: persitence even if user did not configured it
+VOLUME ["/var/logs"]
 
 COPY index.js ./
 
 # Use of ENTRYPOINT to define default command that is not easily overriden
 ENTRYPOINT ["node", "index.js"]
+
+# CMD to give default arguments
+#   Here we could argue that this should be part of the environment.
+CMD ["--log-dir", "/var/logs"]
--- index.js
+++ index.js
@@ -1,23 +1,38 @@
+const fs = require('fs')
 const http = require('http')
 
-// Better: static config as a constant at beginning of code
+// Static config as a constant on top of app
 // This should always be constant, so not really an issue to write here and in Dockerfile.
 const port = 80
 
-// Better: server config can be retrieved from environment. Use a default to avoid early failure if value is not set
+// Server config can be retrieved from environment. Use a default to avoid early failure if value is not set
 const maxHeaderSize = Number.parseInt(process.env.MAX_HEADER_SIZE || 4096)
 
-// Better: start log message
-console.log(
+// Better: parameter to define optional log file
+// Here we could argue that this should be part of the environment.
+let logFile = 2 // 2 is stdout
+let logFileArgIndex = process.argv.indexOf('--log-file')
+if (logFileArgIndex !== -1) {
+  logFile = fs.openSync(process.argv[logFileArgIndex + 1], 'w')
+}
+
+// Start log message
+fs.writeSync(
+  logFile,
   `Starting server on port ${port}; maxHeaderSize is ${maxHeaderSize}\n`
 )
 
 http
   .createServer({ maxHeaderSize: maxHeaderSize }, (req, res) => {
-    res.write('Hello World!\n')
+    const message = 'Hello World!\n'
+    res.write(message)
     res.end()
 
-    // Better: some log messages
-    console.log(`Request from ${req.socket.localAddress}; Sent ${message}\n`)
+    // Some log messages
+    // Better: log to a file meant to be readable outside of Docker
+    fs.writeSync(
+      logFile,
+      `Request from ${req.socket.localAddress}; Sent ${message}\n`
+    )
   })
   .listen(port)
